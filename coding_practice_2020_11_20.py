45ec828c40faf2e8ae51ec0af9eb9fb383b8534a
akyWong114 <54898654+akyWong114@users.noreply.github.com>
2020-11-20 13:55:53 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,9 +1,10 @@
+#question 1 
+
 print("What is your favourite colour: ")
 favourite_Colour = input ()
 print (favourite_Colour, "?! No way, that's my favourite colour too! ")
 
-#don't put have spaces between functions: unless it's * = , or 
-# use _ like "number_Pack" to divide them
+#question 2
 number_Cans_per_Pack = int(input("How many cans per package: "))
 number_Pack = int(input("How many packages are there: "))
 
@@ -16,9 +17,18 @@ print("please enter the length of the length: ")
 length = float (input())
 print ("please enter the width: ")
 
+#question 3
 width = float (input())
 print ("please enter the height: ")
 
 height = float (input())
 volume = length * width * height
 print ("The total volume is" {volume} "mL")
+
+#question 4
+
+user_mutes = input ("do you mute the teacher when you join Google Meet")
+if user_mutes == "no":
+    print("Ok, good")
+else:
+    print("That's probably not a good idea")
