79e656a09764c7403654b1bd01411a589a517a7a
akyWong114 <54898654+akyWong114@users.noreply.github.com>
2020-11-20 14:20:03 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,25 +1,23 @@
 print("What is your favourite colour: ")
 favourite_Colour = input ()
 print (favourite_Colour, "?! No way, that's my favourite colour too! ")
-
 #don't put have spaces between functions: unless it's * = , or 
 # use _ like "number_Pack" to divide them
+
 number_Cans_per_Pack = int(input("How many cans per package: "))
 number_Pack = int(input("How many packages are there: "))
-
 number_Cans = number_Cans_per_Pack * number_Pack
 
 print(f'There are {number_Cans} in a pack. ')
-
 print("please enter the following in cm")
 print("please enter the length of the length: ")
-length = float (input())
+length = float(input())
 print("please enter the width: ")
 
-width = float (input())
+width = float(input())
 print("please enter the height: ")
 
-height = float (input())
+height = float(input())
 volume = length * width * height
 print("The total volume is",volume, "mL.")
 
@@ -31,5 +29,4 @@ if user_mutes == "no":
 else:
     print("That's probably not a good idea")
 
-#make sure your variables makes snese
 
