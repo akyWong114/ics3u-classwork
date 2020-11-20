c268cfb6b1e910dddcc48282f106d8adc71a8898
akyWong114 <54898654+akyWong114@users.noreply.github.com>
2020-11-20 14:17:01 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,10 +1,9 @@
-#question 1 
-
 print("What is your favourite colour: ")
 favourite_Colour = input ()
 print (favourite_Colour, "?! No way, that's my favourite colour too! ")
 
-#question 2
+#don't put have spaces between functions: unless it's * = , or 
+# use _ like "number_Pack" to divide them
 number_Cans_per_Pack = int(input("How many cans per package: "))
 number_Pack = int(input("How many packages are there: "))
 
@@ -12,23 +11,25 @@ number_Cans = number_Cans_per_Pack * number_Pack
 
 print(f'There are {number_Cans} in a pack. ')
 
-print ("please enter the following in cm")
+print("please enter the following in cm")
 print("please enter the length of the length: ")
 length = float (input())
-print ("please enter the width: ")
+print("please enter the width: ")
 
-#question 3
 width = float (input())
-print ("please enter the height: ")
+print("please enter the height: ")
 
 height = float (input())
 volume = length * width * height
-print ("The total volume is", volume, "mL.")
+print("The total volume is",volume, "mL.")
 
 #question 4
 
-user_mutes = input ("do you mute the teacher when you join Google Meet")
+user_mutes = input("do you mute the teacher when you join Google Meet")
 if user_mutes == "no":
     print("Ok, good")
 else:
     print("That's probably not a good idea")
+
+#make sure your variables makes snese
+
