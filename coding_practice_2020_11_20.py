4a6fbf88c40cc6fa35e7db092a041585f6d9d360
akyWong114 <54898654+akyWong114@users.noreply.github.com>
2020-11-22 22:26:43 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -4,12 +4,9 @@ print (favourite_Colour, "?! No way, that's my favourite colour too! ")
 #don't put have spaces between functions: unless it's * = , or 
 # use _ like "number_Pack" to divide them
 
-number_Cans_per_Pack = int(input("How many cans per package: "))
-number_Pack = int(input("How many packages are there: "))
-number_Cans = number_Cans_per_Pack * number_Pack
-
 print(f'There are {number_Cans} in a pack. ')
 print("please enter the following in cm")
+
 length = int(input("please enter the length of the length: "))
 width = int(input("please enter the width: "))
 height = int(input("please enter the height: "))
