f536034c5fb2e3c45876bf164ced370ba3d365ee
akyWong114 <54898654+akyWong114@users.noreply.github.com>
2020-11-27 13:56:48 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,58 @@
+# EXAMPLE
+# 1. from -321 to -188 by 7.
+i = -321
+while i <= -188:
+    print(i)
+    i += 7
+
+#Personal Practice
+#1. from 207 to 223 by 1
+i = 207
+while i <= 223:
+    print(i)
+    i += 1
+#2. from -87 to 1 by 8
+i = -87
+while i <= 1:
+    print(i)
+    i += 8
+#3. from 14 to 60 by 1
+i = 14
+while i <= 60:
+    print(i)
+    i += 1
+#4. from 200 to 485 by 15
+i = 200
+while i <= 485:
+    print(i)
+    i += 15
+#5. from 67 to 389 by 14
+i = 67
+while i <= 389:
+    print(i)
+    i += 14
+#6. from -59 to 58 by 9
+i = -59
+while i <= 58:
+    print(i)
+    i += 9
+#7. from 236 to 556 by 16
+i = 236
+while i <= 556:
+    print(i)
+    i += 16
+#8. from -360 to -272 by 8
+i = -360
+while i <= -272:
+    print(i)
+    i += 8
+#9. from -115 to 173 by 18
+i = -115
+while i <= 173:
+    print(i)
+    i += 18
+#10. from 232 to 522 by 10
+i = <= 232
+while i <= 522:
+    print(i)
+    i += 10
